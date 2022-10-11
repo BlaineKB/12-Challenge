f2dada5bf0d7febca5ce55987c6a308e12a14a04
BKB <blainekb94@gmail.com>
2022-10-10 22:00:13 -0500

    created server/client connection file and employeetracker db

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+// Connects the client
+require('dotenv').config();
+const mysql = require('mysql2');
+const db_user = process.env.DB_USER;
+const db_pass = process.env.DB_PASS;
+
+// Connects to database
+const connection = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: db_user,
+    password: db_pass,
+    database: 'employeetracker_db'
+  }
+)
+
+module.exports = connection;
